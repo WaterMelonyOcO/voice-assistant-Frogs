aaba9ec18d8220ea3c2a8254c5f007a985f4cc68
SoNya Marker <soniamarker13@gmail.com>
2023-09-23 02:08:44 +0300

    login page

--- frontend/voice-assistant/src/App.js
+++ frontend/voice-assistant/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import MainPage from './pages/MainPage/MainPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage/>} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage/>} />
       </Routes>
     </BrowserRouter>
 
