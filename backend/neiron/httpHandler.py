8d77074943edfdf0a1e5979acaccde8e88b96160
WateMelonyOcO <vlad12345678934@gmail.com>
2023-09-22 20:29:34 +0300

    make node api send

--- backend/neiron/httpHandler.py
+++ backend/neiron/httpHandler.py
@@ -1,7 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-
+import neuro
 
 class MyHttpHandler(BaseHTTPRequestHandler):
 
-    def do_GET():
-        pass
+    def do_GET(self):
+        self.send_response(200)
+        self.header("Content-type", "application/json")
+        self.end_headers()
+
+        userRequest = neuro.textVromVoice()
