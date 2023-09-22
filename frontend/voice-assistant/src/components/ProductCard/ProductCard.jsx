80f718a9b3b35c9b58bb79d6f4e64bf2209e3cad
SoNya Marker <soniamarker13@gmail.com>
2023-09-23 00:54:11 +0300

    add a main page + cards

---
+++ frontend/voice-assistant/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,34 @@
+import BtnOutlined from '../BtnOutlined/BtnOutlined';
+import './style.css';
+import heartIconWh from '../../images/heart_white.svg';
+import carttIconWh from '../../images/cart_white.svg';
+import BtnFilled from '../BtnFilled/BtnFilled';
+
+
+export default function ProductCard(product) {
+    
+    let products = Object.values(product)[0];
+    console.log(products)
+    return (
+        <div className='card-body'>
+            <img src={products.image} alt={`картинка товара ${products.name}`}/>
+            <h4 className='card-header' >
+                {products.name}
+            </h4>
+        
+            <span className='price'>
+                <p className='price_before'>
+                {products.price_before} ₽
+                </p>
+                <h5 className='price_after'>
+                    {products.price_after} ₽
+                </h5>
+            </span>
+            
+        {/* <span className='btn-layout'> */}
+            <BtnOutlined name_of_btn={"В избранное"} icon_for_btn={heartIconWh} />
+            <BtnFilled name_of_btn={"Купить"} icon_for_btn={carttIconWh} />
+            {/* </span> */}
+        </div>
+    )
+}
--- frontend/voice-assistant/src/pages/MainPage/MainPage.jsx
+++ frontend/voice-assistant/src/pages/MainPage/MainPage.jsx
@@ -2,13 +2,25 @@ import BtnFilled from "../../components/BtnFilled/BtnFilled";
 import BtnOutlined from '../../components/BtnOutlined/BtnOutlined';
 import Catalog from "../../components/Catalog/Catalog";
 import Header from "../../components/Header/Header";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import "./style.css";
+import dataMainPage from '../../constants/data-main-page.json';
+
 
 export default function MainPage() {
+    let mainData = Object.values(dataMainPage)[0];
     return(<>
         <Header />
-        <Catalog />
-        {/* <BtnFilled name_of_btn={"Напиги мне"} />
-        <BtnOutlined name_of_btn={"Напиги мне"} /> */}
+        <div className="body-layout">
+            <Catalog />
+            <div className="cards-layout">
+                    { mainData.map((product) => (
+                    <ProductCard key={product.id} data={product} />
+                ))}
+            </div>
+        </div>
+        
+            
+        
     </> )
 }
