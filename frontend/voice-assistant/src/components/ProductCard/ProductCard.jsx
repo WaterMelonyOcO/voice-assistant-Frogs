6b4a2e784bc339998a919ec78752b63b42ec4096
SoNya Marker <soniamarker13@gmail.com>
2023-09-23 16:52:15 +0300

    adding images on cards (random)

--- frontend/voice-assistant/src/components/ProductCard/ProductCard.jsx
+++ frontend/voice-assistant/src/components/ProductCard/ProductCard.jsx
@@ -3,15 +3,31 @@ import './style.css';
 import heartIconWh from '../../images/heart_white.svg';
 import carttIconWh from '../../images/cart_white.svg';
 import BtnFilled from '../BtnFilled/BtnFilled';
-
+import test_1 from '../../images/image_1.png';
+import test_2 from '../../images/image_2.png';
+import test_3 from '../../images/image_3.png';
+import test_4 from '../../images/image_4.png';
+import test_5 from '../../images/image_5.png';
+import test_6 from '../../images/image_6.png';
+import test_7 from '../../images/image_7.png';
+import test_8 from '../../images/image_8.png';
+import { useImageToggle } from '../ImageToggleContext/ImageToggleContext';
 
 export default function ProductCard(product) {
+    const randomImages = [test_1, test_2, test_3, test_4, test_5, test_6, test_7, test_8];
+    const randomIndex = Math.floor(Math.random() * randomImages.length);
+    const randomImage = randomImages[randomIndex];
+
+    const { areImagesVisible } = useImageToggle();
     
     let products = Object.values(product)[0];
     console.log(products)
     return (
         <div className='card-body'>
-            <img src={products.image} alt={`картинка товара ${products.name}`}/>
+            {areImagesVisible && (
+        <img src={randomImage} alt={`картинка товара ${products.name}`}/>
+      )}
+            
             <h4 className='card-header' >
                 {products.name}
             </h4>
