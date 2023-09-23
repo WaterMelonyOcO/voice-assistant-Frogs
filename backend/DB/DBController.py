a3377995853e6d69a36d13cc5509edb3cb9b20a1
WateMelonyOcO <vlad12345678934@gmail.com>
2023-09-23 10:27:21 +0300

    start create db

---
+++ backend/DB/DBController.py
@@ -0,0 +1,12 @@
+
+from peewee import *
+
+
+class DBController:
+
+    def __init__(self) -> None:
+        # self.dbFile = databaseFile
+        # self.dbFileConnection = SqliteDatabase(self.dbFile)
+        # self.dbCursore = self.dbFileConnection.cursor()
+        pass
+    
